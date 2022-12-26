9be42da1848083265b6a8c2fed99457beafaecc5
Imbeshat <md.imbeshat@gmail.com>
2022-12-27 01:17:09 +0530

    added js file for basic calculator app

---
+++ 03functionsAndObjects/04basicCalculatoFunc.js
@@ -0,0 +1,10 @@
+function calc(num1, num2) {
+	console.log(`Sum of ${num1} and ${num2} is`, num1 + num2);
+	console.log(`Difference of ${num1} and ${num2} is`, num1 - num2);
+	console.log(`Multiplication of ${num1} and ${num2} is`, num1 * num2);
+	console.log(`Division of ${num1} and ${num2} is`, num1 / num2);
+	console.log(`Modulus of ${num1} and ${num2} is`, num1 % num2);
+	console.log("-----------------------------------");
+}
+calc(20, 10);
+calc(30, 8);
