68052c25b32da162ab90909f89735b99a0f7da84
Imbeshat <md.imbeshat@gmail.com>
2022-12-28 04:52:55 +0530

    added datatypes js file

---
+++ 01basics/02dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // works on browser not on nodejs
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null - It is a standalone value
+// undefined - varible is declared but value is not given
+// symbol => used for uniqueness
+
+// object
+
+console.log(typeof null); // typeof null is object
+console.log(typeof undefined); // typeof undefined is undefined
