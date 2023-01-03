88bd56175f1e456055807890e80568eb266385b1
Imbeshat <md.imbeshat@gmail.com>
2023-01-03 16:17:13 +0530

    added practice js file for date and math

---
+++ 01basics/07dateAndMath.js
@@ -0,0 +1,36 @@
+// Date
+
+const date = new Date();
+console.log(date);
+console.log(date.toString());
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+
+console.log(date.getTime());
+
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+
+// Math
+
+const PI = Math.PI;
+console.log(PI);
+
+console.log(Math.round(PI));
+console.log(Math.round(9.8));
+console.log(Math.floor(9.5));
+console.log(Math.ceil(9.5));
+
+console.log(Math.min(10, 1, 2, 3, 5));
+console.log(Math.max(10, 1, 2, 3, 5));
+
+console.log(Math.round(Math.random() * 100)); // 0 - 0.99999999999 (0-1)
+
+console.log(Math.pow(3, 3));
+
+console.log(Math.log(2));
+
+console.log(Math.sin(90));
