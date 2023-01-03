15878d71fff768ac3e45cc609d908785d11de1dc
Imbeshat <md.imbeshat@gmail.com>
2023-01-04 00:00:51 +0530

    practice js files of basic loops

---
+++ 01basics/08basicLoops.js
@@ -0,0 +1,21 @@
+// Loops
+// do while, while, for
+
+// do while
+let i = 6;
+do {
+	console.log("Value of i is :", i);
+	i++;
+} while (i <= 10);
+
+// while
+let j = 1;
+while (j <= 5) {
+	console.log("Value of j is ", j);
+	j++;
+}
+
+// for
+for (let i = 0; i <= 10; i++) {
+	console.log(`${i} * ${i} = ${i * i}`);
+}
