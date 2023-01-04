8c21d4eaccf8e439ceab8c77e61409d9a9953fc0
Imbeshat <md.imbeshat@gmail.com>
2023-01-05 01:09:03 +0530

    updated splice array method js file

--- 02arrayMethods/05spliceArrayMethod.js
+++ 02arrayMethods/05spliceArrayMethod.js
@@ -3,6 +3,6 @@ let superHero = ["SpiderMan", "IronMan", "Batman", "SuperMan", "Thor"];
 console.log("Array before splice: ", superHero);
 
 // replaces 1 element at index 4
-superHero.splice(4, 1, "Hulk");
-
+superHero.splice(4, 1, "Hulk"); // item at index 4 is replaced
+// superHero.splice(4, 0, "Hulk"); // no item is replaced
 console.log("Array before splice: ", superHero);
