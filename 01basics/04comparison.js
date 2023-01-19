4cf0e819008ce11093b072bb17e0107d45dfd123
Imbeshat <md.imbeshat@gmail.com>
2023-01-20 00:52:58 +0530

    added practice file for comparison

---
+++ 01basics/04comparison.js
@@ -0,0 +1,25 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2>=1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null <= 0); // comparison treats null as 0
+// console.log(null == 0); // equality in javascript works differently. It does not treat null as 0.
+// console.log(null != 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+// console.log(undefined == 0);
+
+// Strict cjeck (===)
+console.log("2" === 2);
