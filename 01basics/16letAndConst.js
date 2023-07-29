92a277f29ec1c3d2acb698832e833023e907f4e7
Imbeshat <md.imbeshat@gmail.com>
2023-07-29 22:39:53 +0530

    practice file for let and const

---
+++ 01basics/16letAndConst.js
@@ -0,0 +1,6 @@
+let a = 10;
+//let a = 20; // SyntaxError: Identifier 'a' has already been declared
+
+// const b; // SyntaxError: Missing initializer in const declaration
+const b = 100;
+// b = 200; // TypeError: Assignment to constant variable.
