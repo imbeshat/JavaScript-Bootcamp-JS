a2165108c6993e9d1bec8dcb2af2ed49feaf77e1
Imbeshat <md.imbeshat@gmail.com>
2023-08-17 10:50:46 +0530

    event listener along with closure

--- 01basics/30callbackFunction.js
+++ 01basics/30callbackFunction.js
@@ -17,3 +17,13 @@ document.getElementById("clickMe").addEventListener("click", function xyz() {
 	console.log("Button Clicked");
 });
 // Here function xyz is a callback function. It is called when the button is clicked and is pushed in the callstack and is executed.
+
+// Closure along with event listener
+function attachEventListener() {
+	let count = 0;
+	document.getElementById("clickMe").addEventListener("click", function xyz() {
+		console.log("Button Clicked", ++count);
+	});
+}
+attachEventListener();
+// Here the function xyz is a callback function. It is called when the button is clicked and is pushed in the callstack and is executed. But the variable count is not in the scope of the function xyz. So it will look for the variable count in the outer scope/lexical scope. It will find the variable count in the outer scope and will increment it. This is called closure.
